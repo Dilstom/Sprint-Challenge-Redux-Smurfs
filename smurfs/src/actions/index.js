352bdead69f1726ad32aaae3632cc0b03052d6b7
Dil Sombayeva <dilyasom@gmail.com>
2019-06-27 16:10:16 -0700

    implement getSmurfs action creator

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -17,3 +17,15 @@ export const SUCCESS = 'SUCCESS';
    U - updateSmurf
    D - deleteSmurf
 */
+
+export const getSmurfs = () => dispatch => {
+ dispatch({ type: FETCH_START });
+ axios
+  .get('http://localhost:3333/smurfs')
+  .then(res => {
+   //    console.log('checking res: ', res);
+   dispatch({ type: FETCH_SUCCESS, payload: res.data });
+  })
+  .catch(err => dispatch({ type: ERROR, payload: err }));
+};
+
