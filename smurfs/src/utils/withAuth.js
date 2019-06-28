a55c4819b59390203a501c01198d4bfca2db7ad2
Dil Sombayeva <dilyasom@gmail.com>
2019-06-28 12:18:39 -0700

    comment out a line of code

--- smurfs/src/utils/withAuth.js
+++ smurfs/src/utils/withAuth.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const withAuth = () => {
  return axios.create({
   headers: {
-   'Content-Type': 'application/json',
+   //    'Content-Type': 'application/json',
    Authorization: localStorage.getItem('userToken'),
   },
   baseURL: 'http://localhost:3333/api',
