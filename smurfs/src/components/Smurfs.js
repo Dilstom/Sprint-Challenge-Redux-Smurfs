0aea80b5156b10e006c3a64aaba08f9fca0d8f59
Dil Sombayeva <dilyasom@gmail.com>
2019-06-28 12:15:55 -0700

    Smurfs gets App.js code

---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import './App.css';
+import { connect } from 'react-redux';
+import { getSmurfs } from '../actions';
+import Smurf from './Smurf';
+import AddSmurfForm from './AddSmurfForm';
+
+/*
+ to wire this component up you're going to need a few things.
+ I'll let you do this part on your own. 
+ Just remember, `how do I `connect` my components to redux?`
+ `How do I ensure that my component links the state to props?`
+ */
+class Smurfs extends Component {
+ componentDidMount() {
+  this.props.getSmurfs();
+ }
+
+ render() {
+  return (
+   <div className="App">
+    <AddSmurfForm />
+    {this.props.fetchingSmurfs ? (
+     <h3>Loading...</h3>
+    ) : (
+     <div>
+      {this.props.smurfs.map(smurf => {
+       return <Smurf key={smurf.id} smurf={smurf} />;
+      })}
+     </div>
+    )}
+   </div>
+  );
+ }
+}
+
+const mapStateToProps = state => {
+ console.log('state', state);
+ return {
+  smurfs: state.smurfs,
+  fetchingSmurfs: state.fetchingSmurfs,
+ };
+};
+
+export default connect(
+ mapStateToProps,
+ { getSmurfs }
+)(Smurfs);
