3f69547b03d4e737429440c379d13817f7495732
Dil Sombayeva <dilyasom@gmail.com>
2019-06-28 12:12:16 -0700

    create headers with Authorization and baseURL

---
+++ smurfs/src/utils/withAuth.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const withAuth = () => {
+ return axios.create({
+  headers: {
+   'Content-Type': 'application/json',
+   Authorization: localStorage.getItem('userToken'),
+  },
+  baseURL: 'http://localhost:3333/api',
+ });
+};
+
+export default withAuth;
