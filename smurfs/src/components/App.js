892c69b71644ed38325491e2402fcbab20288aa3
Dil Sombayeva <dilyasom@gmail.com>
2019-06-28 12:14:43 -0700

    refactor App.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import { connect } from 'react-redux';
-import { getSmurfs } from '../actions';
-import Smurf from './Smurf';
-import AddSmurfForm from './AddSmurfForm';
+import Smurfs from './Smurfs';
+import LoginForm from './LoginForm';
+import { Route, Link } from 'react-router-dom';
+import PrivateRoute from './PrivateRoute';
 
 /*
  to wire this component up you're going to need a few things.
@@ -12,10 +12,12 @@ import AddSmurfForm from './AddSmurfForm';
  `How do I ensure that my component links the state to props?`
  */
 class App extends Component {
- componentDidMount() {
-  this.props.getSmurfs();
+ constructor() {
+  super();
+  this.state = {
+   auth: false,
+  };
  }
-
  render() {
   return (
    <div className="App">
@@ -33,15 +35,4 @@ class App extends Component {
  }
 }
 
-const mapStateToProps = state => {
- console.log('state', state);
- return {
-  smurfs: state.smurfs,
-  fetchingSmurfs: state.fetchingSmurfs,
- };
-};
-
-export default connect(
- mapStateToProps,
- { getSmurfs }
-)(App);
+export default App;
